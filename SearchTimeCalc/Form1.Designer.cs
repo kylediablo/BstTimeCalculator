f71e8649b163b1371b8ef6a5105f0f17c665f08a
kjdiablo <kjdiablo@J108-08>
2017-11-20 13:15:48 -0800

    Add project files.

---
+++ SearchTimeCalc/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SearchTimeCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.outLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(41, 105);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(312, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(253, 67);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(38, 70);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(209, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Enter number of records to search through:";
+            // 
+            // outLabel
+            // 
+            this.outLabel.AutoSize = true;
+            this.outLabel.Location = new System.Drawing.Point(12, 178);
+            this.outLabel.Name = "outLabel";
+            this.outLabel.Size = new System.Drawing.Size(48, 13);
+            this.outLabel.TabIndex = 3;
+            this.outLabel.Text = "outLabel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(403, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Calculate search time using a Binary Search Tree";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outLabel);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Calculate Search Time App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label outLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SearchTimeCalc/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchTimeCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            outLabel.Visible = false;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // on click, check if input is an int, then make the call to calculate and output to a label
+            int recordsInput;
+            if (int.TryParse(txtInput.Text, out recordsInput))
+            {
+                NumberOfSearches(recordsInput, outLabel);
+                outLabel.Visible = true;
+            }
+            else
+                MessageBox.Show("Please enter a valid number");
+                  
+
+                
+
+        }
+
+        private void NumberOfSearches(int records, Label label)
+        {
+            // number of searches is euqal to Log2(x); where x is the number of records
+            int searches = Convert.ToInt32(Math.Log(records, 2));
+            label.Text = string.Format("{0} searches at the most to find a specific record.", searches);
+        }
+    }
+}
